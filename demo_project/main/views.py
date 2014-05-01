951cffff6bd86131949bb7241f704b714d127eb4
taccat <TiffanyAnnChen@Macintosh-5.local>
2014-04-30 20:16:48 -0400

    boo1

--- demo_project/main/views.py
+++ demo_project/main/views.py
@@ -19,13 +19,14 @@ def home(request):
     if query_string:
         query_string = query_string.upper()
         reverse_complement = ''
-        if query_string not in base:
-            context ['query_string'] = query_string
-            context ['reverse_complement'] = "Here's a good link to learn more: http://en.wikipedia.org/wiki/DNA_bases"
-        else:
-            for base in query_string:
+        for base in query_string:
+            if base not in BASE_PAIRING:
+                context ['reverse_complement'] = "Not-bases detected. Here's a good link to learn more: http://en.wikipedia.org/wiki/DNA_bases"
+                return render(request, 'home.html', context)
+            else:
                 reverse_complement += BASE_PAIRING[base]
-            context['query_string'] = query_string
-            context['reverse_complement'] = reverse_complement
-
+        context['query_string'] = query_string
+        context['reverse_complement'] = reverse_complement
     return render(request, 'home.html', context)
+
+    
