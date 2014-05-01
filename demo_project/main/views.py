24254cf5502bbc9388d93c03f442d39bdc880d4f
taccat <TiffanyAnnChen@Macintosh-5.local>
2014-04-30 22:36:52 -0400

    spacingspacing yay!

--- demo_project/main/views.py
+++ demo_project/main/views.py
@@ -14,6 +14,7 @@ BASE_PAIRING = {
 
 # thanks waynesword.palomar.edu
 dnaToAminoAcid = {
+    # Dictionary of DNA codons to amino Acids
     'CGA': 'alanine',
     'CGG': 'alanine',
     'CGT': 'alanine',
@@ -82,6 +83,8 @@ dnaToAminoAcid = {
 
 
 def converter(baseSequence):
+
+    # Converts DNA codon to amino acid according to dictionary.
     counter = 1
     codon = ''
     returnedSequence = ''
@@ -102,31 +105,31 @@ def home(request):
     context = {}
 
     # Check whether this request includes a query string to translate.
-    # query_string = request.GET.get('query', None)
-    # if query_string:
-    #     query_string = query_string.upper()
-    #     reverse_complement = ''
-    #     for base in query_string:
-    #         reverse_complement += BASE_PAIRING[base]
-    #     context['query_string'] = query_string
-    #     context['reverse_complement'] = reverse_complement
-
     query_string = request.GET.get('query', None)
     if query_string:
         query_string = query_string.upper()
         reverse_complement = ''
+
         for base in query_string:
+
+            # checks if base in allowed bases; returns error message if not.
             if base not in BASE_PAIRING:
                 context[
                     'reverse_complement'] = "Not-bases detected. Here's a good link to learn more: http://en.wikipedia.org/wiki/DNA_bases"
                 return render(request, 'home.html', context)
+
             else:
+
+                # quick base pairing.
                 reverse_complement += BASE_PAIRING[base]
+
+                # runs converter on converted bases.
                 aa = converter(reverse_complement)
                 if aa == '':
                     context['amino_acids'] = 'Sorry, not enough bases.'
                 else:
                     context['amino_acids'] = aa
+
         context['query_string'] = query_string
         context['reverse_complement'] = reverse_complement
 
