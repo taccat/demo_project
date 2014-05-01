a1567bc6e16bab701e004926abd5dd94869d3174
taccat <TiffanyAnnChen@Macintosh-5.local>
2014-04-30 20:39:37 -0400

    CAT
    MEOW

--- demo_project/main/views.py
+++ demo_project/main/views.py
@@ -14,4 +14,16 @@ BASE_PAIRING = {
 
 
 def home(request):
-    return HTTPResponse('OHAI')
+    context = {}
+
+    # Check whether this request includes a query string to translate.
+    query_string = request.GET.get('query', None)
+    if query_string:
+        query_string = query_string.upper()
+        reverse_complement = ''
+        for base in query_string:
+            reverse_complement += BASE_PAIRING[base]
+        context['query_string'] = query_string
+        context['reverse_complement'] = reverse_complement
+
+    return render(request, 'home.html', context)
