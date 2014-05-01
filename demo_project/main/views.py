fd62e43b9cc4549eb04e7301cb57f61282e028f9
taccat <TiffanyAnnChen@Macintosh-5.local>
2014-04-30 21:20:33 -0400

    let's goooo

--- demo_project/main/views.py
+++ demo_project/main/views.py
@@ -17,12 +17,26 @@ def home(request):
     context = {}
 
     # Check whether this request includes a query string to translate.
+    # query_string = request.GET.get('query', None)
+    # if query_string:
+    #     query_string = query_string.upper()
+    #     reverse_complement = ''
+    #     for base in query_string:
+    #         reverse_complement += BASE_PAIRING[base]
+    #     context['query_string'] = query_string
+    #     context['reverse_complement'] = reverse_complement
+
     query_string = request.GET.get('query', None)
     if query_string:
         query_string = query_string.upper()
         reverse_complement = ''
         for base in query_string:
-            reverse_complement += BASE_PAIRING[base]
+            if base not in BASE_PAIRING:
+                context[
+                    'reverse_complement'] = "Not-bases detected. Here's a good link to learn more: http://en.wikipedia.org/wiki/DNA_bases"
+                return render(request, 'home.html', context)
+            else:
+                reverse_complement += BASE_PAIRING[base]
         context['query_string'] = query_string
         context['reverse_complement'] = reverse_complement
 
