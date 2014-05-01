4075a9e60816d163880a94e3993648c924f41fb9
taccat <TiffanyAnnChen@Macintosh-5.local>
2014-04-30 20:03:13 -0400

    boo

--- demo_project/main/views.py
+++ demo_project/main/views.py
@@ -19,9 +19,13 @@ def home(request):
     if query_string:
         query_string = query_string.upper()
         reverse_complement = ''
-        for base in query_string:
-            reverse_complement += BASE_PAIRING[base]
-        context['query_string'] = query_string
-        context['reverse_complement'] = reverse_complement
+        if query_string not in base:
+            context ['query_string'] = query_string
+            context ['reverse_complement'] = "Here's a good link to learn more: http://en.wikipedia.org/wiki/DNA_bases"
+        else:
+            for base in query_string:
+                reverse_complement += BASE_PAIRING[base]
+            context['query_string'] = query_string
+            context['reverse_complement'] = reverse_complement
 
     return render(request, 'home.html', context)
