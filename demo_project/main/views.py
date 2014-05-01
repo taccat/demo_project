f5135eaaabccff4c381089bf3783a9798936daeb
taccat <TiffanyAnnChen@Macintosh-5.local>
2014-04-30 20:38:05 -0400

    I AM A PENGUIN

--- demo_project/main/views.py
+++ demo_project/main/views.py
@@ -3,6 +3,7 @@ Views.
 """
 
 from django.shortcuts import render
+from django.http import HTTPResponse
 
 BASE_PAIRING = {
     'A': 'T',
@@ -13,20 +14,4 @@ BASE_PAIRING = {
 
 
 def home(request):
-    context = {}
-
-    # Check whether this request includes a query string to translate.
-    query_string = request.GET.get('query', None)
-    if query_string:
-        query_string = query_string.upper()
-        reverse_complement = ''
-        for base in query_string:
-            if base not in BASE_PAIRING:
-                context[
-                    'reverse_complement'] = "Not-bases detected. Here's a good link to learn more: http://en.wikipedia.org/wiki/DNA_bases"
-                return render(request, 'home.html', context)
-            else:
-                reverse_complement += BASE_PAIRING[base]
-        context['query_string'] = query_string
-        context['reverse_complement'] = reverse_complement
-    return render(request, 'home.html', context)
+    return HTTPResponse('OHAI')
